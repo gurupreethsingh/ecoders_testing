c414d13f9873a2d40c8af455ad4ce1c8793fea90
igurupreeth <igurupreeth@gmail.com>
2024-12-30 11:25:11 +0530

    java project created, start pattern programs.

---
+++ Core_java_topics/src/star_patterns/Patten_template.java
@@ -0,0 +1,23 @@
+package star_patterns;
+
+public class Patten_template {
+
+	public static void main(String[] args) 
+	{
+		for(int i = 1; i<=5 ; i++)
+		{
+			for(int j= 1; j<=5; j++)
+			{
+				if(j<= i)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
