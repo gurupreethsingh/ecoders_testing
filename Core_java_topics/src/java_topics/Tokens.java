5dd0be8f720e53ef931b9aa78af85a95a9025cb5
igurupreeth <igurupreeth@gmail.com>
2024-12-30 13:26:13 +0530

    Tokens java class added to the project.

---
+++ Core_java_topics/src/java_topics/Tokens.java
@@ -0,0 +1,26 @@
+// tokens - are smallest unit of any program , programming language. 
+/*
+ * 1. identifiers and datatypes.
+ * 2. keywords
+ * 3. varaibles
+ * 4.separators
+ * 5. operators. 
+ * (Arithematic, relational, logical , incremental , decrimental, assignment.)
+ * 
+ * */
+
+package java_topics;
+
+public class Tokens 
+{
+  public static void main(String[] args) {
+	System.out.println(10);
+	System.out.println(10.34);
+	System.out.println(true);
+	System.out.println(false);
+	System.out.println('a');
+	System.out.println("hello");
+	System.out.println("12433");
+	System.out.println('4');
+}
+}
