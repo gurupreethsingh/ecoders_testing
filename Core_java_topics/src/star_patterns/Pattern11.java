59c912761f958ddae46a0b3e864e0e226ac37a28
sneha758 <sneha456tr@gmail.com>
2024-12-30 14:02:37 +0530

    pattern11 added to project

---
+++ Core_java_topics/src/star_patterns/Pattern11.java
@@ -0,0 +1,28 @@
+package star_patterns;
+
+public class Pattern11 {
+
+	public static void main(String[] args)
+	{
+		int b = 1;
+		for(int i = 1; i<=5; i++)
+		{
+			int a = 1;
+			for(int j=1; j<=6; j++)
+			{
+				if(j%2==0)
+				{
+					System.out.print(a);
+					a = a+1;
+				}
+				else
+				{
+					System.out.print(b);
+				}
+			}
+			System.out.println();
+			b = b+1;
+		}
+
+	}
+}
