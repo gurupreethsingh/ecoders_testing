72ff724f060b583e7b66a495013ad201525fbd8a
igurupreeth <igurupreeth@gmail.com>
2024-12-30 15:28:30 +0530

    pattern 13

---
+++ Core_java_topics/src/star_patterns/Pattern_13_2.java
@@ -0,0 +1,29 @@
+package star_patterns;
+
+public class Pattern_13_2 {
+
+	public static void main(String[] args) 
+	{
+		int num = 1; int num2 = 10;
+		for(int i = 1; i<=5 ; i++)
+		{
+			int k = num;  int k2 = num2;
+			for(int j= 1; j<=5; j++)
+			{
+				if(j%2!=0)
+				{
+					System.out.print(k+" ");
+					k = k+10;
+				}
+				else
+				{
+					System.out.print(k2+" ");
+					k2 = k2+10;
+				}
+			}
+			System.out.println();
+			num = num +1;
+			num2 = num2-1;
+		}
+	}
+}
