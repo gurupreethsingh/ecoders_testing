a0b3422a1c416857bf8d8a43e1aedf683e360921
Lokesh Daksh <lokeshdaksh09@gmail.com>
2024-12-30 14:07:57 +0530

    Pattern12 program added

---
+++ Core_java_topics/src/star_patterns/Pattern12.java
@@ -0,0 +1,22 @@
+package star_patterns;
+
+public class Pattern12 {
+
+	public static void main(String[] args) {
+		{
+			int num = 1;
+			for(int i = 1; i<=5 ; i++)
+			{
+				int k = num;
+				for(int j= 1; j<=6; j++)
+				{
+					System.out.print(k +" ");
+					k=k+5;																
+				}
+				System.out.println();
+				num =num+1;
+			}
+		}
+	}
+}
+
