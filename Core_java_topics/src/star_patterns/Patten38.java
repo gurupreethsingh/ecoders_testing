54f5e9c435ecd69e4bf40bcf4d03124e5e3edf73
Veenakrishna10 <veenapb55@gmail.com>
2024-12-30 20:24:32 +0530

    Pattern38 added to the project

---
+++ Core_java_topics/src/star_patterns/Patten38.java
@@ -0,0 +1,26 @@
+
+package star_patterns;
+
+public class Patten38 {
+
+	public static void main(String[] args) 
+	{
+		for(int i = 1; i<=5 ; i++)
+		{
+			int num=5;
+			for(int j= 1; j<=5; j++)
+			{
+				if(j<=i)
+				{
+					System.out.print(num+" ");
+					num--;
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
