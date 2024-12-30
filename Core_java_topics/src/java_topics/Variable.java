1d83970f72149bb30b67e2d9a264fdffe4c99cf8
igurupreeth <igurupreeth@gmail.com>
2024-12-30 14:40:36 +0530

    vairalbes class added

---
+++ Core_java_topics/src/java_topics/Variable.java
@@ -0,0 +1,35 @@
+// variables are named memory locations. which can store any data and 
+// can be changed during execution of the program.
+
+package java_topics;
+
+public class Variable {
+
+	public static void main(String[] args) 
+	{
+		byte a = 1;
+		short b = 2; 
+		char ch = 'a';
+		int num = 23;
+		// float varaible has to be ended with f at the end. 
+		float f = 3.3f; 
+		// long variable has to be ended with l at the end.
+		long l = 23465345387698762l;
+		double d = 45.34;
+		boolean result = true; 
+		String s1 = "hello world";
+		String s2 = "1232abc";
+		String s3 = "234563462";
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(ch);
+		System.out.println(num);
+		System.out.println(f);
+		System.out.println(l);
+		System.out.println(d);
+		System.out.println(result);
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+	}
+}
