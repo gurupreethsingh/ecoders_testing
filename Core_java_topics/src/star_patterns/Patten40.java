c2af4e24e403e3e7223f77ec719f2c1ee442186a
Veenakrishna10 <veenapb55@gmail.com>
2024-12-30 20:32:20 +0530

    Pattern40 added to the project

---
+++ Core_java_topics/src/star_patterns/Patten40.java
@@ -0,0 +1,26 @@
+
+package star_patterns;
+
+public class Patten40 {
+
+	public static void main(String[] args) 
+	{
+		for(int i = 1; i<=5 ; i++)
+		{
+			int num=2;
+			for(int j= 1; j<=5; j++)
+			{
+				if(j<=i)
+				{
+					System.out.print(num+" ");
+					num=num+2;
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
