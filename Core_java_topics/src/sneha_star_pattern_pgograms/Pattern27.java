03e46613a3faa0efdde0d3db6d8296eb8cc965e8
sneha758 <sneha456tr@gmail.com>
2024-12-31 18:58:40 +0530

    program27 added

--- Core_java_topics/src/sneha_star_pattern_pgograms/Pattern27.java
+++ Core_java_topics/src/sneha_star_pattern_pgograms/Pattern27.java
@@ -1,19 +1,20 @@
 package sneha_star_pattern_pgograms;
 
-public class Pattern26 {
+public class Pattern27 {
 
 	public static void main(String[] args)
-	{ int num1=65;
+	{ 
 		for(int i = 1; i<=5 ; i++)
 		{
+			int num1=65;
+
 			for(int j= 1; j<=5; j++)
 			{
 				
 					System.out.print((char)num1);
-				
-			}
+					num1=num1+1;
+						}
 			System.out.println();
-			num1=num1+1;
 		}
        }
 }
