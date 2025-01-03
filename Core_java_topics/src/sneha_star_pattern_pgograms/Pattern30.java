c9f305b7dbc2f7c8c29409ac9a7c96b6381ad7b3
sneha758 <sneha456tr@gmail.com>
2025-01-03 18:33:55 +0530

    pattern30 added

---
+++ Core_java_topics/src/sneha_star_pattern_pgograms/Pattern30.java
@@ -0,0 +1,18 @@
+package sneha_star_pattern_pgograms;
+
+public class Pattern30 {
+
+	public static void main(String[] args)
+	{ 
+		int num1=64;
+		for(int i = 1; i<=5 ; i++)
+		{
+			 for(int j= 1; j<=5; j++)
+			       {
+				  num1=num1+1;
+					System.out.print((char)num1);
+			       }
+			 System.out.println();
+			}
+       }
+}
