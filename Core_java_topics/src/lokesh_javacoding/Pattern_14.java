4293a28d563e71108d1a9cb3033024360569cdb5
Lokesh Daksh <lokeshdaksh09@gmail.com>
2025-01-08 01:15:24 +0530

    pattern 14 added

---
+++ Core_java_topics/src/lokesh_javacoding/Pattern_14.java
@@ -0,0 +1,31 @@
+package lokesh_javacoding;
+
+public class Pattern_14 
+{
+	public static void main(String[] args) 
+	{
+		int num = 5; int num2 = 10;
+		for(int i = 1; i<=5 ; i++)
+		{
+			int k = num;  int k2 = num2;
+			for(int j= 1; j<=5; j++)
+			{
+				if(j%2!=0)
+				{
+					System.out.print(k+" ");
+					k = k+10;
+				}
+				else
+				{
+					System.out.print(k2+" ");
+					k2 = k2+10;
+				}
+			}
+			System.out.println();
+			num = num -1;
+			num2 = num2-1;
+		}
+	}
+
+
+}
