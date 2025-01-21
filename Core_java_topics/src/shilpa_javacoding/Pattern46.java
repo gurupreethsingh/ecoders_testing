9107ee2881ce784a58c4e19442a9f76295bdf76a
NShilpamurthhy <nshilpamurthy@gmail.com>
2025-01-21 13:19:34 +0530

    pattern 46 added

---
+++ Core_java_topics/src/shilpa_javacoding/Pattern46.java
@@ -0,0 +1,28 @@
+
+package shilpa_javacoding;
+
+public class Pattern46{
+
+	public static void main(String[] args) 
+	{
+		int num=2;
+		for(int i = 1; i<=5 ; i++)
+		{
+			int k=num;
+			for(int j= 1; j<=5; j++)
+			{	
+				if(j<=i)
+				{
+					System.out.print(k+" ");
+					k=k+2;
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+			num=num+2;	
+		}
+	}
+}
