b11f2e402cb52f52c47e90f9a7dcaacb71d9b39d
Lokesh Daksh <lokeshdaksh09@gmail.com>
2025-01-21 13:05:14 +0530

    footer update

---
+++ Core_java_topics/src/all_blogs_scenarios/O34_OpenHomePageFromFooterABp.java
@@ -0,0 +1,75 @@
+package all_blogs_scenarios;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class O34_OpenHomePageFromFooterABp {
+	
+	public static WebDriver driver; 
+
+	public static void main(String[] args) throws InterruptedException
+	{
+		// open chrome browser.
+  	  driver = new ChromeDriver();  
+      System.out.println("Chrome browser opened successfully.");
+      
+      driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+      // maximize the window. 
+      driver.manage().window().maximize();
+      
+      // go to homepage 
+      driver.get("http://localhost:5173/all-blogs");
+//      driver.get("http://localhost:5173/");
+//      driver.get("http://localhost:5173/home");
+//      driver.get("http://localhost:5173/homepage");    not working.
+      
+      String homePageHandle = driver.getWindowHandle();
+      System.out.println("Homepage Window Handle: " + homePageHandle);
+      
+      String actualUrl = driver.getCurrentUrl();
+      System.out.println(actualUrl);
+      driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+      
+      JavascriptExecutor js = (JavascriptExecutor) driver;
+
+    
+          Thread.sleep(1000);
+          js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+          Thread.sleep(1000);
+          
+          //find the Homepage link. 
+        	 WebElement homepage =  driver.findElement(By.xpath("//a[@class='text-white font-semibold hover:text-red-800 transition'][normalize-space()='Home']"));
+        	 homepage.click();
+        	 Thread.sleep(1000);
+        	 
+        	 Set<String> windowHandles = driver.getWindowHandles();
+        	 
+             List<String> windowHandlesList = new ArrayList<String>(windowHandles);
+        	 
+             for (String windowHandle : windowHandlesList) {
+                 driver.switchTo().window(windowHandle);
+                 System.out.println("windown number" + windowHandle);
+                 System.out.println("Window Title: " + driver.getTitle());
+                 System.out.println("Window URL: " + driver.getCurrentUrl());
+                 System.out.println("*********************************************");
+             }
+        	 
+        	 System.out.println("title of homepage page"+ driver.getTitle());
+        	 Thread.sleep(1000);
+        	 
+        	    driver.switchTo().window(homePageHandle);
+                System.out.println("Switched back to homepage.");
+                System.out.println("Title of Home Page: " + driver.getTitle());
+          
+      Thread.sleep(5000);
+      driver.quit();
+	}
+}
